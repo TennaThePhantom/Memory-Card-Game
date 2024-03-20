1e446e58024994b5a6ff5118cfa3fa58e8bcc6fb
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2024-03-20 15:40:20 -0400

    timer works

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const cards = document.querySelectorAll(".grid-image-card");
+let seconds = 0;
+let minutes = 0;
+let timerInterval;
 
-cards.forEach((card) => {
-    card.addEventListener("click", () => {
-        card.classList.toggle("card-is-flipped");
-    });
-});
+function startClock() {
+	timerInterval = setInterval(updateClock, 1000);
+}
+
+function stopClock() {
+	clearInterval(timerInterval);
+}
+
+function updateClock() {
+	seconds++;
+	if (seconds === 60) {
+		seconds = 0;
+		minutes++;
+	}
+	clockTimer();
+}
+
+function clockTimer() {
+	window.onload = startClock;
+	const formattedTime = formatTime(minutes) + ":" + formatTime(seconds);
+	document.getElementById("clock").textContent = formattedTime;
+}
+
+function formatTime(time) {
+	return time < 10 ? "0" + time : time;
+}
+function flipCards() {
+	cards.forEach((card) => {
+		card.addEventListener("click", () => {
+			card.classList.toggle("card-is-flipped");
+		});
+	});
+}
+
+flipCards();
+clockTimer();
